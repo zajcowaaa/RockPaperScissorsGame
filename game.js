235dbd9368f4342f77e265644f0138a47901e140
Anna Nowakowska <anna.izabela.nowakowska@gmail.com>
2021-02-22 16:45:13 +0100

    initial commit

---
+++ game.js
@@ -0,0 +1,92 @@
+
+
+const gameSummary = {
+    numbers: 0,
+    wins: 0,
+    losses: 0,
+    draws: 0,
+}
+
+const game = {
+    playerHand: "",
+    aiHand: "",
+}
+
+const hands = [...document.querySelectorAll('.select img')];
+
+// First Function
+function handSelection() {
+
+    game.playerHand = this.dataset.option
+    console.log(game.playerHand);
+    hands.forEach(hand => hand.style.boxShadow = '');
+    this.style.boxShadow = '0 0 0 4px yellow';
+}
+
+// const handSelection = (e) => {
+//  // this - nie tworzy
+//  console.log(e.target);
+//  console.log(e.currentTarget);
+// }
+
+// Computer choice function
+function aiChoice() {
+    return hands[Math.floor(Math.random() * 3)].dataset.option;
+}
+
+// Function with data results
+function checkResult(player, ai) {
+    // console.log(player, ai);
+    if (player === ai) {
+        return 'draw';
+    } else if ((player === "papier" && ai === "kamień") || (player === "kamień" && ai === "nożyczki") || (player === "nożyczki" && ai === "papier")) {
+        return 'win';
+    } else {
+        return 'loss';
+    }
+}
+
+// Results
+
+function publishResult(player, ai, result) {
+    document.querySelector('[data-summary="your-choice"]').textContent = player;
+
+    document.querySelector('[data-summary="ai-choice"]').textContent = ai;
+
+    document.querySelector('p.numbers span').textContent = ++gameSummary.numbers;
+
+    if (result === "win") {
+        document.querySelector('p.wins span').textContent = ++gameSummary.wins;
+        document.querySelector('[data-summary="who-win"]').textContent = "You won!! :)"
+        document.querySelector('[data-summary="who-win"]').style.color = "green";
+    } else if (result === "loss") {
+        document.querySelector('p.losses span').textContent = ++gameSummary.losses;
+        document.querySelector('[data-summary="who-win"]').textContent = "Computer wins :("
+        document.querySelector('[data-summary="who-win"]').style.color = "red";
+    } else {
+        document.querySelector('p.draws span').textContent = ++gameSummary.draws;
+        document.querySelector('[data-summary="who-win"]').textContent = "That's a tie ://"
+        document.querySelector('[data-summary="who-win"]').style.color = "gray";
+    }
+}
+
+function endGame() {
+    document.querySelector(`[data-option="${game.playerHand}"]`).style.boxShadow = "";
+    game.playerHand = "";
+    game.aiHand = "";
+}
+//Steering Function 
+function startGame() {
+    if (!game.playerHand) {
+        return alert("You have to choose.");
+    }
+    game.aiHand = aiChoice();
+    const gameResult = checkResult(game.playerHand, game.aiHand);
+    console.log(gameResult);
+    publishResult(game.playerHand, game.aiHand, gameResult);
+    endGame()
+}
+
+hands.forEach(hand => hand.addEventListener('click', handSelection))
+
+document.querySelector('.start').addEventListener('click', startGame)
